4fafa99ab9e849b5fe1b9aa7085b6f04bd38e561
diosamuel <koding12sam@gmail.com>
2023-06-08 13:52:48 +0700

    fix overflow desktop

--- Swarnakasa ITERA/script.js
+++ Swarnakasa ITERA/script.js
@@ -22,14 +22,14 @@ menuBtn.onclick = function () {
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
+  // body.style.overflow = "hidden";
   scrollBtn.style.pointerEvents = "none";
 };
 cancelBtn.onclick = function () {
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
   menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
+  // body.style.overflow = "auto";
   scrollBtn.style.pointerEvents = "auto";
 };
 
